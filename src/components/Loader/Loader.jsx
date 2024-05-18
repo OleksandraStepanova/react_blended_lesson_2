40bb0a4ce7ec65d0ed3a551161521eb7749e7afb
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-05-18 17:16:55 +0300

    text-commit

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,6 @@
+import style from './Loader.module.css'
+import BeatLoader from "react-spinners/BeatLoader";
+
 export const Loader = () => {
-  return <div>Loader</div>;
+  return <div className={style.backdrop}><BeatLoader color="#36d7b7" /></div>;
 };

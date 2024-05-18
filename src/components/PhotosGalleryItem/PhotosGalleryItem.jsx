dcc5f806eac8418da7fdceab424c7d64fb846971
Natalie-Dor <varnata000@gmail.com>
2024-05-18 16:29:23 +0300

    update

--- src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
+++ src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
@@ -1,3 +1,14 @@
-export const PhotosGalleryItem = () => {
-  return <h3>PhotosGalleryItem</h3>;
+import { GridItem } from '..';
+import style from './PhotosGalleryItem.module.css';
+export const PhotosGalleryItem = ({ galleryItem: { avg_color, src, alt } }) => {
+  return (
+    <GridItem>
+      <div
+        className={style.thumb}
+        style={{ backgroundColor: avg_color, borderColor: avg_color }}
+      >
+        <img src={src.large} alt={alt} />
+      </div>
+    </GridItem>
+  );
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,31 +1,33 @@
 import { getPhotos } from 'apiService/photos';
-import { Form, Text } from 'components';
+import { Form, Text, PhotosGallery } from 'components';
 import { Toaster } from 'react-hot-toast';
 import { useEffect, useState } from 'react';
 
 export const Photos = () => {
-  const [query, setQuery] = useState('')
-  const [page, setPage] = useState(1)
-  const [gallery, setGallery] = useState([])
-  
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [gallery, setGallery] = useState([]);
+
   useEffect(() => {
     if (!query) return;
-    const handleGallery = async() => {
-    const value= await getPhotos(query, page)
-      setGallery(value.photos);      
-    }
+    const handleGallery = async () => {
+      const value = await getPhotos(query, page);
+      setGallery(value.photos);
+    };
     handleGallery();
-  },[query, page])
+  }, [query, page]);
 
-  const getQuery = (value) => {
-    setQuery(value)
-  }
+  const getQuery = value => {
+    setQuery(value);
+  };
 
   return (
     <>
       <Form onSubmit={getQuery} />
-      <Text textAlign="center">Let`s begin search 🔎</Text>
-      <Toaster/>
+      <PhotosGallery gallery={gallery} />
+      {query === '' && <Text textAlign="center">Let`s begin search 🔎</Text>}
+
+      <Toaster />
     </>
   );
 };
